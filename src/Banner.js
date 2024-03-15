0f1289e57e5f0eee749ddb2abb7d42d974873efb
ramonornelas <ornelas@pythian.com>
2024-03-14 18:05:15 -0600

    magus1.02

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,7 @@ import codeBanner from './code_banner.png'; // Import the image file
 const Banner = () => {
   return (
     <div>
-      <img src={codeBanner} />
+      <img src={codeBanner} alt="Banner" />
     </div>
   );
 };
