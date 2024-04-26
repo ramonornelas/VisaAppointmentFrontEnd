39a2c253b456abeaae625500c8213352e26b0826
ramonornelas <ornelas@pythian.com>
2024-04-25 22:03:43 -0600

    Atreides3.0

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import Banner from './Banner';
-import HamburgerMenu from './HamburgerMenu';
-import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 const Welcome = ({ username }) => {
   return (
     <div>
-      <HamburgerMenu />
-      <div style={{ marginBottom: '5px' }}></div>
-      <Banner />
       <div style={{ marginBottom: '5px' }}></div>
       <h2>Welcome, {username}!</h2>
       <p>You have been registered successfully</p>
-      <Footer />
+      <Link to="/">Go to Home to log in</Link>
     </div>
   );
 };
