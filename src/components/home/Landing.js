70261fa3f3d230f8d1172287719d160d8bb39a78
Ramon Ornelas <ramonornelas@gmail.com>
2026-02-07 15:06:12 -0600

    Add landing page component and remove unused Home/Welcome components

---
+++ src/components/home/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  React.useEffect(() => {
+    window.location.href = '/landing/index.html';
+  }, []);
+ 
+  return null;
+};
+
+export default Landing;
